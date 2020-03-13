197625838b189253abfb32e76b5b43f5bbfbac41
Abanoub Wahib <abanoub.wagdy.dev@gmail.com>
2020-03-13 11:39:56 +0200

    remove dummy injection for fragment support

--- app/src/main/java/com/parentAps/di/AppInjector.kt
+++ app/src/main/java/com/parentAps/di/AppInjector.kt
@@ -3,13 +3,8 @@ package com.parentAps.di
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import com.parentAps.App
 import dagger.android.AndroidInjection
-import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.HasSupportFragmentInjector
 
 object AppInjector {
 
@@ -50,28 +45,6 @@ object AppInjector {
     }
 
     private fun handleActivity(activity: Activity) {
-//        if (activity is HasSupportFragmentInjector) {
-//            AndroidInjection.inject(activity)
-//        }
-//        if (activity is FragmentActivity) {
-//            activity.supportFragmentManager
-//                .registerFragmentLifecycleCallbacks(
-//                    object : FragmentManager.FragmentLifecycleCallbacks() {
-//                        override fun onFragmentCreated(
-//                            fm: FragmentManager,
-//                            f: Fragment,
-//                            savedInstanceState: Bundle?
-//                        ) {
-//                            if (f is Injectable) {
-//                                AndroidSupportInjection.inject(f)
-//                            }
-//                        }
-//                    }, true
-//                )
-//        } else if (activity is Injectable) {
-//            AndroidInjection.inject(activity)
-//        }
-
         if (activity is Injectable) {
             AndroidInjection.inject(activity)
         }
